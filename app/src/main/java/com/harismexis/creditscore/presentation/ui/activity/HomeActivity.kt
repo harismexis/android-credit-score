b8d878d0784f71a9257e88a3f557f2311845076d
Haris Mexis <theoharismexis@hotmail.com>
2021-07-26 19:00:07 +0100

    customised status bar

--- app/src/main/java/com/harismexis/creditscore/presentation/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/harismexis/creditscore/presentation/ui/activity/HomeActivity.kt
@@ -1,21 +1,33 @@
 package com.harismexis.creditscore.presentation.ui.activity
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.harismexis.creditscore.R
 import com.harismexis.creditscore.presentation.ui.fragment.HomeFragment
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        renderStatusBar()
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
                 add<HomeFragment>(R.id.fragment_container_view)
             }
         }
+    }
+
+    private fun renderStatusBar() {
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(this, android.R.color.white)
+        val wic = WindowInsetsControllerCompat(window, window.decorView)
+        wic.isAppearanceLightStatusBars = true
     }
 }
