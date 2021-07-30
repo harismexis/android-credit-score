9c48d04096eb0f26681a3f20cba78a71fc24b89c
Haris Mexis <theoharismexis@hotmail.com>
2021-07-30 17:53:05 +0100

    updates

--- app/src/main/java/com/harismexis/creditscore/presentation/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/harismexis/creditscore/presentation/home/viewmodel/HomeViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.harismexis.creditscore.core.domain.CreditReport
 import com.harismexis.creditscore.core.data.repository.CreditRepository
 import com.harismexis.creditscore.core.result.CreditResult
 import com.harismexis.creditscore.framework.event.Event
@@ -38,12 +37,8 @@ class HomeViewModel @Inject constructor(
     fun getCreditReport() {
         viewModelScope.launch {
             mCanRefresh.value = false
-
-            var report: CreditReport?
-
-            // fetch, emit & save CreditReport
             try {
-                report = repository.getRemoteCreditReport()
+                val report = repository.getRemoteCreditReport()
                 report?.let {
                     mCredit.value = CreditResult.Success(it)
                     repository.save(it)
@@ -51,28 +46,25 @@ class HomeViewModel @Inject constructor(
             } catch (e: Exception) {
                 val errMsg = e.msg()
                 Log.d(TAG, errMsg)
-                report = null
-                // show error that update failed
                 mShowSnackBar.value = Event(errMsg)
+                emitCachedCreditReport()
             }
-
-            // if get remote failed fetched latest cached
-            if (report == null) {
-                try {
-                    report = repository.getLocalCreditReport()
-                    if (report != null) {
-                        mCredit.value = CreditResult.Success(report)
-                    } else {
-                        mCredit.value =
-                            CreditResult.Error(Exception("Failed to retrieve CreditScore"))
-                    }
-                } catch (ex: Exception) {
-                    Log.d(TAG, ex.msg())
-                    mCredit.value = CreditResult.Error(ex)
-                }
-            }
-
             mCanRefresh.value = true
+        }
+    }
+
+    private suspend fun emitCachedCreditReport() {
+        try {
+            val report = repository.getLocalCreditReport()
+            if (report != null) {
+                mCredit.value = CreditResult.Success(report)
+            } else {
+                mCredit.value =
+                    CreditResult.Error(Exception("No cached CreditScore"))
+            }
+        } catch (ex: Exception) {
+            Log.d(TAG, ex.msg())
+            mCredit.value = CreditResult.Error(ex)
         }
     }
 
